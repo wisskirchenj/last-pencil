751c71f15a3a22a1ad3d8a66cf62c883aba986e3
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2022-10-15 11:55:22 +0200

    stage 2 completed

--- src/lastpencil/last_pencil.py
+++ src/lastpencil/last_pencil.py
@@ -1,2 +1,5 @@
-print('|||||')
-print('Your turn!')
+
+pencil_count = int(input('How many pencils would you like to use:\n'))
+first = input('Who will be the first (Mary, Joseph):\n')
+print('|' * pencil_count)
+print(f'{first} is going first')
