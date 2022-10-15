7cf1ae8642a3107821be9a1c819833aca4367e2b
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2022-10-15 12:28:22 +0200

    stage 3 completed

--- src/lastpencil/last_pencil.py
+++ src/lastpencil/last_pencil.py
@@ -1,5 +1,27 @@
+players = ["Mary", "Joseph"]
 
-pencil_count = int(input('How many pencils would you like to use:\n'))
-first = input('Who will be the first (Mary, Joseph):\n')
-print('|' * pencil_count)
-print(f'{first} is going first')
+
+def prompt_pencil_count(prompt_text):
+    return int(input(prompt_text))
+
+
+def prompt_turn_player():
+    return input(f'Who will be the first ({players[0]}, {players[1]}):\n')
+
+
+def do_turn():
+    global pencil_count, turn_player
+    print('|' * pencil_count)
+    print(f"{turn_player}'s turn")
+    pencil_count -= prompt_pencil_count("")
+    turn_player = players[1] if turn_player == players[0] else players[0]
+
+
+pencil_count = prompt_pencil_count("How many pencils would you like to use:\n")
+turn_player = prompt_turn_player()
+while turn_player not in players:
+    print(f'Choose either {players[0]} or {players[1]}!')
+    turn_player = prompt_turn_player()
+
+while pencil_count > 0:
+    do_turn()
